fe8b82dc558371ba845c7eb694111b872fc7d3b1
Tony Xie <tx248@nyu.edu>
2016-11-27 21:21:55 -0800

    emote recognition broken

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,23 +1,51 @@
 import Ember from 'ember';
 
+
 export default Ember.Route.extend({
   userFromParams: null,
 
 model(params) {
   this.set('userFromParams', params.user_name);
-  return this.store.findAll('message');
+  window.store = this.store;
+
+  return Ember.RSVP.hash({
+    messages: this.store.query('message', {limitToLast: 5}),
+    emotes: this.store.findAll('emote')
+  });
 },
 
   actions: {
     createMessage(message) {
+      const checkEmotes = (str) => {
+        let arr = str.split(" ");
+        let newArr = [];
+        arr.forEach((word) => {
+          // console.log(word);
+          this.store.query('emote', {name: `${word}`}).then((emote) => {
+            console.log(emote);
+          });
+
+        });
+        // console.log(newArr);
+        return newArr.join(" ");
+      };
+
       let newRecord = this.store.createRecord('message', {
-        text: message,
+        text: checkEmotes(message),
         user: this.get('userFromParams')
       });
 
       newRecord.save();
 
       this.controller.set('textMessageFromInput', '');
+    },
+    saveKappa(){
+      // let newKappa = this.store.createRecord('emote', {
+      //   name: "PogChamp",
+      //   url: "<img src='https://static-cdn.jtvnw.net/emoticons/v1/88/1.0'></img>"
+      // });
+      //
+      // newKappa.save();
     }
   }
 });
