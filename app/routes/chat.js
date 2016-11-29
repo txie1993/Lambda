adbc0324632f3b206b5add71cda3c7b8be6fdca4
Tony Xie <tx248@nyu.edu>
2016-11-28 19:56:24 -0800

    emote replacement almost there

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,33 +1,34 @@
 import Ember from 'ember';
+import DS from "ember-data";
 
 
 export default Ember.Route.extend({
   userFromParams: null,
 
-model(params) {
-  this.set('userFromParams', params.user_name);
-  window.store = this.store;
+  model(params) {
+    this.set('userFromParams', params.user_name);
 
-  return Ember.RSVP.hash({
-    messages: this.store.query('message', {limitToLast: 5}),
-    emotes: this.store.findAll('emote')
-  });
-},
+    return Ember.RSVP.hash({
+      messages: this.store.query('message', {
+        limitToLast: 5
+      }),
+      emotes: this.store.findAll('emote')
+    });
+  },
 
   actions: {
     createMessage(message) {
+
       const checkEmotes = (str) => {
-        let arr = str.split(" ");
-        let newArr = [];
-        arr.forEach((word) => {
-          // console.log(word);
-          this.store.query('emote', {name: `${word}`}).then((emote) => {
-            console.log(emote);
+        return this.store.query('emote', {}).then((result) => {
+
+          result.forEach(e => {
+            let re = new RegExp(`${e.get('name')}`, "g");
+            str = str.replace(re, `${e.get('url')}`);
           });
 
+          return str;
         });
-        // console.log(newArr);
-        return newArr.join(" ");
       };
 
       let newRecord = this.store.createRecord('message', {
@@ -39,7 +40,7 @@ model(params) {
 
       this.controller.set('textMessageFromInput', '');
     },
-    saveKappa(){
+    saveKappa() {
       // let newKappa = this.store.createRecord('emote', {
       //   name: "PogChamp",
       //   url: "<img src='https://static-cdn.jtvnw.net/emoticons/v1/88/1.0'></img>"
